1613926afe5c5e10847bdfecfb67465dd20f2009
Mayra1225 <mayra.vargas@udla.edu.ec>
2025-07-07 07:31:21 -0500

    Creacion del Modelo Producto y servicio ProductosServices para la conexion con la base de datos

---
+++ VargasMExamenFinal/Services/ProductosService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VargasMExamenFinal.Models;
+
+namespace VargasMExamenFinal.Services
+{
+    public class ProductosService
+    {
+        private readonly SQLite.SQLiteAsyncConnection _connection;
+
+        public ProductosService(string dbPath)
+        {
+            _connection = new SQLite.SQLiteAsyncConnection(dbPath);
+            _connection.CreateTableAsync<Models.Producto>().Wait();
+        }
+
+        public Task<List<Producto>> GetProductoAsync()
+        {
+            return _connection.Table<Producto>().ToListAsync();
+        }
+
+        public Task<int> SaveProdcutoAsync(Producto producto)
+        {
+            if (producto.Id != 0)
+            {
+                return _connection.UpdateAsync(producto);
+            }
+            else
+            {
+                return _connection.InsertAsync(producto);
+            }
+        }
+    }
+}
