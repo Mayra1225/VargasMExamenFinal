ae6ab5ae4d55f99aabeb1c143d8d3c72911e050f
Mayra1225 <mayra.vargas@udla.edu.ec>
2025-07-07 07:06:26 -0500

    Agregar archivos de proyecto.

---
+++ VargasMExamenFinal/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VargasMExamenFinal
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ VargasMExamenFinal/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace VargasMExamenFinal
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
